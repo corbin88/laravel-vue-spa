aa4d6f14d255e7870314f869741c7f56f69fbd94
Kyle Hurst <kyle.c.hurst@gmail.com>
2020-05-01 04:37:23 -0400

    Add backend authorization login, register, and logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+
+        $request->validate([
+            'username' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $req = Request::create(route('passport.token'), 'POST', [
+
+            'grant_type' => 'password',
+            'client_id' => config('services.passport.client_id'),
+            'client_secret' => config('services.passport.client_secret'),
+            'username' => $request->username,
+            'password' => $request->password,
+            'scope' => ''
+        ]);
+    
+        return $response = app()->handle($req);
+
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        return User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens->each(function ($token, $key) {
+            $token->delete();
+        });
+
+        return response()->json('Logged out successfully', 200);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/{any}', 'app')->where('any', '.*');
+Route::get('{path}', function () {
+    return view('app');
+})->where('path', '(.*)');
